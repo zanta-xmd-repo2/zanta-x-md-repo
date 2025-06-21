51d42ca151a7fe75778d4d3c67e21de35c5571df
zanta-xmd-repo2 <surangawhatsappbotxd@gmail.com>
2025-06-21 20:19:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || " ",
-MONGODB: process.env.MONGODB || " "
-
+SESSION_ID: process.env.SESSION_ID || "Put the session-id here.", // ඔයාගේ session id එක දාන්න
+MONGODB: process.env.MONGODB || "Put the mongodb url here.", // ඔයාගේ mongodb url එක දාන්න
+ALIVE_IMG: process.env.ALIVE_IMG || "YOUR PHOTO URL",
+BOT_NAME: process.env.BOT_NAME || "MD",
+LANG: process.env.BOT_LANG || 'EN' ,
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
+DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
 };
