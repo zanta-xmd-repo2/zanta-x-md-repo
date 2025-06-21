4388da78ca5be21a09b2b33ee3c95d6929237037
zanta-xmd-repo2 <surangawhatsappbotxd@gmail.com>
2025-06-21 20:27:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const ownerNumber = ['94760663483']
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID.split("MD=")[1];
+const sessdata = config.SESSION_ID.split("ZANTA-XMD=")[1];
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
